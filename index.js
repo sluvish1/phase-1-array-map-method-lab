13b479451eeb5c2958ad4cd498b5a67ff37dbc53
Vaschell Mkenzie <mvaschell@gmail.com>
2023-04-18 17:41:08 -0400

    hard

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = (first) => {
+  let cap = tutorials.map((first) => {
+    first.split(` `)
+    return first
+    .split(` `)
+    .map((word) => word[0].toLocaleUpperCase() + word.substring(1))
+    .join(` `)
+  })
+  return cap }
